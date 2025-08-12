1ffb20892b0c77f6a2c618c6223e2cfc16f7304f
Vinogradov Dmitriy <vinogradovdima123456789@gmail.com>
2025-08-12 12:16:31 +0500

    Поместить файл конфигурации в gitignor

--- backend/src/main/java/ru/ogma/utils/PropertiesSinglton.java
+++ backend/src/main/java/ru/ogma/utils/PropertiesSinglton.java
@@ -8,12 +8,12 @@ import java.util.Properties;
 public class PropertiesSinglton {
     private static Properties properties;
 
-    public PropertiesSinglton(String locationConfFile) throws IOException {
-        properties = new Properties();
-        properties.load(new FileInputStream(locationConfFile));
-    }
-
     public static Properties getInstance() throws IOException {
         return properties;
+    }
+
+    public static void addLocationConfigFile(String locationConfFile) throws IOException {
+        properties = new Properties();
+        properties.load(new FileInputStream(locationConfFile));
     }
 }
