4d030b12598b2741cf98b8ef4d34482e01899d92
Vinogradov Dmitriy <vinogradovdima123456789@gmail.com>
2025-08-13 16:46:25 +0500

    Настроить логирование в utils

--- backend/src/main/java/ru/ogma/utils/PersonValidator.java
+++ backend/src/main/java/ru/ogma/utils/PersonValidator.java
@@ -1,11 +1,17 @@
 package ru.ogma.utils;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.ogma.entities.Person;
 
 public class PersonValidator {
+
+    private static final Logger logger = LoggerFactory.getLogger(PersonValidator.class);
+
     public static boolean isPersonValidate(Person person) throws NullPointerException {
         if (person == null) {
+            logger.error("Person is null");
             throw new NullPointerException("Person is null");
         }
         return isNameCorrect(person) && isEmailCorrect(person);
@@ -13,9 +19,11 @@ public class PersonValidator {
 
     private static boolean isNameCorrect(Person person) throws NullPointerException {
         if (person.getUsername() == null) {
+            logger.error("Person.username is null");
             throw new NullPointerException("Person.Username is null");
         }
         if (person.getUsername().length() > 50 || person.getUsername().isEmpty()) {
+            logger.debug("Person.username инвалидный");
             return false;
         }
         return true;
@@ -23,8 +31,13 @@ public class PersonValidator {
 
     private static boolean isEmailCorrect(Person person) throws NullPointerException {
         if (person.getEmail() == null) {
+            logger.error("Person.email is null");
             throw new NullPointerException("Person.Email is null");
         }
-        return EmailValidator.getInstance().isValid(person.getEmail());
+        if (EmailValidator.getInstance().isValid(person.getEmail())){
+            return true;
+        }
+        logger.debug("Person.email инвалидный");
+        return false;
     }
 }
